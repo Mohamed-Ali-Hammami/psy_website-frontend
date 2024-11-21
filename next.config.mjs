9f4e85fb4bcf9d6d119e27d37d1aab0075741d3a
Mohamed-Ali-Hammami <99651583+Mohamed-Ali-Hammami@users.noreply.github.com>
2024-11-21 22:12:02 +0100

    updated

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,25 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['cdn.sanity.io', 'localhost', 'localhost:5000'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'cdn.sanity.io',
+      },
+      {
+        protocol: 'http',  // or 'https', depending on your local setup
+        hostname: 'localhost',
+      },
+      {
+        protocol: 'http',  // or 'https', depending on your local setup
+        hostname: 'localhost',
+        port: '5000',
+      },
+      {
+        protocol: 'https',  // assuming your deployed site uses https
+        hostname: 'psywebsite-frontend-production.up.railway.app',
+      },
+    ],
   },
   transpilePackages: ['@mui/material'],
 };
